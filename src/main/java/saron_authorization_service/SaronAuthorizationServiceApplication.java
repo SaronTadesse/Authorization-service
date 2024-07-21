523c5c57571daf0232f8a1f96483b88507a76741
sarontadesse <sarontadesse456@gmail.com>
2024-07-20 22:06:17 -0500

    initial commit

---
+++ src/main/java/saron_authorization_service/SaronAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package saron_authorization_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaronAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaronAuthorizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/saron_authorization_service/SaronAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package saron_authorization_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaronAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
